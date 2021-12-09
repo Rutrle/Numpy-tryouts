8027d0bc51f08f1efe3100e82d45d813a302646f
Jakub Rutrle <rutrle@seznam.cz>
2021-12-09 22:27:25 +0100

    indexing

--- basics.py
+++ basics.py
@@ -10,7 +10,7 @@ def print_array_info(np_array, array_name):
     print(f"itemsize : {np_array.itemsize} /bytes")
     print(f"total number of elements: {np_array.size}")
     print(np_array)
-    print('_________________________________________________________')
+    print('_'*50)
 
 
 a = np.array([1, 2, 3])
@@ -20,3 +20,36 @@ c = np.array([5, 7, 4], dtype='int16')
 print_array_info(a, 'a')
 print_array_info(b, 'b')
 print_array_info(c, 'c')
+
+
+d = np.array([[1, 2, 3, 4, 5, 6, 7, 16], [8, 9, 10, 11, 12, 13, 14, 15]])
+print(d)
+# getting specific element - [r,c]
+
+print(d[0, 0])
+print(d[-1, -2])
+print(d[0, :])
+print(d[:, 3])
+print(d[0, 1:6])
+print(d[0, 1:6:2])
+print(d[0, 1::2])
+
+d[0, 0] = 265
+print(d)
+
+d[:, 2] = 5
+print(d)
+
+d[:, 2] = [1000, 1111]
+print(d)
+
+print('_'*50 + '3d')
+d3 = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+
+print(d3)
+
+print(d3[0, 1, 1])
+
+d3[:, 1, :] = [[9, 9], [8, 8]]
+
+print(d3)
